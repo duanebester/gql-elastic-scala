b2be3d483dc6aede2a8d1ee1d7872dcf37f73757
duanebester <bester.duane@gmail.com>
2019-12-02 13:01:52 -0800

    cors handler

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,7 +9,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json._
 import scala.language.postfixOps
 
-object Main extends App {
+object Main extends App with CORSHandler {
   val PORT = 8080
   implicit val actorSystem = ActorSystem("graphql-server")
   implicit val materializer = ActorMaterializer()
@@ -19,13 +19,13 @@ object Main extends App {
   scala.sys.addShutdownHook(() -> shutdown())
 
   val route: Route =
-    (post & path("graphql")) {
+    corsHandler((post & path("graphql")) {
       entity(as[JsValue]) { requestJson =>
         GraphQLServer.endpoint(requestJson)
       }
     } ~ {
       getFromResource("graphiql.html")
-    }
+    })
 
   Http().bindAndHandle(route, "0.0.0.0", PORT)
   println(s"GraphiQL Available at: http://localhost:$PORT")
@@ -34,4 +34,35 @@ object Main extends App {
     actorSystem.terminate()
     Await.result(actorSystem.whenTerminated, 30 seconds)
   }
+}
+
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.server.{ Directive0, Route }
+import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
+import akka.http.scaladsl.model.headers._
+
+trait CORSHandler {
+  private val corsResponseHeaders = List(
+    `Access-Control-Allow-Origin`.*,
+    `Access-Control-Allow-Credentials`(true),
+    `Access-Control-Allow-Headers`(
+      "Authorization",
+      "Content-Type",
+      "X-Requested-With"
+    )
+  )
+  private def addAccessControlHeaders: Directive0 =
+    respondWithHeaders(corsResponseHeaders)
+  private def preflightRequestHandler: Route = options {
+    complete(
+      HttpResponse(StatusCodes.OK).withHeaders(
+        `Access-Control-Allow-Methods`(OPTIONS, POST, PUT, GET, DELETE)
+      )
+    )
+  }
+  def corsHandler(r: Route): Route = addAccessControlHeaders {
+    preflightRequestHandler ~ r
+  }
+  def addCORSHeaders(response: HttpResponse): HttpResponse =
+    response.withHeaders(corsResponseHeaders)
 }
